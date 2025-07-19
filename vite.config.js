ff348585a6cd84283d064f203dd4ad6c9081f5c7
Isaac Leong <isaacleong23367@gmail.com>
2025-07-19 10:41:20 +1000

    Updated Vite Config for Github Action Deploy

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/landing-page-2/',
   build: {
     target: 'es2020',
     minify: 'esbuild',
